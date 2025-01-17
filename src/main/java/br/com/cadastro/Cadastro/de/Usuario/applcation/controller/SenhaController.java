87bdb87d6d016af53d7097ba676584b437f00cda
Silas Felipe Garcia <Silas@MacBook-Air-de-Mac.local>
2025-01-16 21:29:42 -0300

    ajustando nonmecaltura e padronizacao rest

--- src/main/java/br/com/cadastro/Cadastro/de/Usuario/applcation/controller/SenhaController.java
+++ src/main/java/br/com/cadastro/Cadastro/de/Usuario/applcation/controller/SenhaController.java
@@ -1,4 +1,9 @@
 package br.com.cadastro.Cadastro.de.Usuario.applcation.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/senhas")
 public class SenhaController {
 }
--- src/main/java/br/com/cadastro/Cadastro/de/Usuario/applcation/controller/UsuarioController.java
+++ src/main/java/br/com/cadastro/Cadastro/de/Usuario/applcation/controller/UsuarioController.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/usuario")
+@RequestMapping("/usuarios")
 public class UsuarioController {
 
 
@@ -25,7 +25,7 @@ public class UsuarioController {
         this.usuarioService = usuarioService;
     }
 
-    @PostMapping("/cadastrar")
+    @PostMapping
     @Transactional
     public ResponseEntity<Void> cadastrarUsuario(@RequestBody @Validated UsuarioForm usuario){
         this.usuarioService.cadastrarUsuario(usuario.converter());

70c4ff54db8dc6b6e97369cc4a824b6d46d73d75
Silas Felipe Garcia <Silas@MacBook-Air-de-Mac.local>
2025-02-23 21:21:48 -0300

    incluindo em um serviço

--- src/main/java/br/com/cadastro/Cadastro/de/Usuario/applcation/service/SenhaService.java
+++ src/main/java/br/com/cadastro/Cadastro/de/Usuario/applcation/service/SenhaService.java
@@ -3,6 +3,7 @@ package br.com.cadastro.Cadastro.de.Usuario.applcation.service;
 import br.com.cadastro.Cadastro.de.Usuario.applcation.form.SenhaForm;
 import br.com.cadastro.Cadastro.de.Usuario.domain.entity.Usuario;
 import br.com.cadastro.Cadastro.de.Usuario.domain.service.ISenhaService;
+import br.com.cadastro.Cadastro.de.Usuario.infra.client.OpenApiClient;
 import br.com.cadastro.Cadastro.de.Usuario.infra.repository.UsuarioRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -16,8 +17,11 @@ class SenhaService implements ISenhaService {
 
     private UsuarioRepository usuarioRepository;
 
-    public SenhaService(UsuarioRepository usuarioRepository) {
+    private OpenApiClient openApiClient;
+
+    public SenhaService(UsuarioRepository usuarioRepository, OpenApiClient openApiClient) {
         this.usuarioRepository = usuarioRepository;
+        this.openApiClient = openApiClient;
     }
 
     @Override
@@ -29,6 +33,9 @@ class SenhaService implements ISenhaService {
         }
         Usuario usuario = usuarioOptional.get();
         usuario.setSenha(senha.converter());
+
+        String dados = this.openApiClient.getDados();
+        System.out.println(dados);
 
         usuarioRepository.save(usuario);
 
